2dd4e2250d27dc43b90ba482af5fee6c6e9d91ff
Pascal Machard <pascal.machard@gmail.com>
2015-03-29 19:06:59 +0200

    update with c++ macro

--- src/cpp_math_test.cpp
+++ src/cpp_math_test.cpp
@@ -1,5 +1,4 @@
 // cpp_math_test.cpp : main project file.
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,8 +20,8 @@ public :
 
 	void TestConstructors(void)
 	{
-		try
-		{
+		BEGIN_UNIT_TEST("TestConstructors")
+
 			SimpleVector2D * pv3;
 			SimpleVector2D v1;
 			SimpleVector2D v2(1.0, 2.0);
@@ -31,40 +30,117 @@ public :
 			Check(v1.GetX() == 0.0 && v1.GetY() == 0.0,"Error in the default constuctor");
 			Check(v2.GetX() == 1.0 && v2.GetY() == 2.0, "Error in the constructor with 2 parameter");
 			Check(pv3->GetX() == 1.0 && pv3->GetY() == 2.0, "Error in the constuctor with copy parameter");
-		}
-		catch (...)
-		{
-			std::cout << "Error when run TestSimpleVector2D_TestConstructor\n";
-		}
+		END_UNIT_TEST("TestConstructors")
 	}
 
 	void TestOperatorAffectation(void)
 	{
-		try
-		{
-			SimpleVector2D v1(1.0, 2.0);
-			SimpleVector2D v2 = v1;
+		BEGIN_UNIT_TEST("TestOperatorAffectation")
 
-			Check(v2.GetX() == 1.0 && v2.GetY() == 2.0, "Error in affection operator");
-		}
-		catch (...)
-		{
-			std::cout << "<Error>when run TestSimpleVector2D_TestOperatorAffectation</Error>\n";
-		}
+			SimpleVector2D v1(1.0, 2.0);
+			SimpleVector2D v2,v3;
+			v3 = v2 = v1;
+
+			Check(v2.GetX() == v1.GetX() && v2.GetY() == v1.GetY(), "Error in affection operator");
+			Check(v3.GetX() == v1.GetX() && v3.GetY() == v1.GetY(), "Error in affection operator");
+
+		END_UNIT_TEST("TestOperatorAffectation")
+	}
+
+	void TestAbs(void)
+	{
+		BEGIN_UNIT_TEST("TestABs")
+
+			SimpleVector2D v1(-1.0, -2.0);
+			SimpleVector2D v2 = v1.abs();
+
+			CheckEqual(v2.GetX(),-v1.GetX(), "Error in affection Abs function");
+			CheckEqual(v2.GetY(),-v1.GetY(), "Error in affection Abs function");
+
+		END_UNIT_TEST("TestABs")
+	}
+
+	void TestOperatorAdd(void)
+	{
+		BEGIN_UNIT_TEST("TestOperatorAdd")
+
+			SimpleVector2D v1(1.5, -2.3);
+			SimpleVector2D v2(0.7, 0.33);
+			SimpleVector2D v3 = v1 + v2;
+			SimpleVector2D v4 = v1;
+			v4+=v2;
+
+			CheckEqual(v3.GetX(), v1.GetX() + v2.GetX() , "Error in additional operator");
+			CheckEqual(v3.GetY(), v1.GetY() + v2.GetY() , "Error in additional operator");
+			CheckEqual(v4.GetX(), v1.GetX() + v2.GetX() , "Error in additional operator");
+			CheckEqual(v4.GetY(), v1.GetY() + v2.GetY() , "Error in additional operator");
+
+		END_UNIT_TEST("TestOperatorAdd")
+	}
+
+	void TestOperatorSupp(void)
+	{
+		BEGIN_UNIT_TEST("TestOperatorSupp")
+
+			SimpleVector2D v1(1.5, -2.3);
+			SimpleVector2D v2(0.7, 0.33);
+			SimpleVector2D v3 = v1 - v2;
+			SimpleVector2D v4 = v1;
+			v4-=v2;
+
+			CheckEqual(v3.GetX(), v1.GetX() - v2.GetX() , "Error in diff operator");
+			CheckEqual(v3.GetY(), v1.GetY() - v2.GetY() , "Error in diff operator");
+			CheckEqual(v4.GetX(), v1.GetX() - v2.GetX() , "Error in diff operator");
+			CheckEqual(v4.GetY(), v1.GetY() - v2.GetY() , "Error in diff operator");
+
+		END_UNIT_TEST("TestOperatorSupp")
+	}
+
+	void TestOperatorMult(void)
+	{
+		BEGIN_UNIT_TEST("TestOperatorMult")
+
+			SimpleVector2D v1(1.5, -2.3);
+			SimpleVector2D v2 = v1 * 0.75;
+			SimpleVector2D v3 = v1;
+			v3*=0.75;
+
+			CheckEqual(v2.GetX(), v1.GetX() * 0.75 , "Error in multiplication operator");
+			CheckEqual(v2.GetY(), v1.GetY() * 0.75 , "Error in multiplication operator");
+			CheckEqual(v3.GetX(), v1.GetX() * 0.75 , "Error in multiplication operator");
+			CheckEqual(v3.GetY(), v1.GetY() * 0.75 , "Error in multiplication operator");
+
+		END_UNIT_TEST("TestOperatorMult")
+	}
+
+	void TestOperatorDiv(void)
+	{
+		BEGIN_UNIT_TEST("TestOperatorDiv")
+
+			SimpleVector2D v1(1.5, -2.3);
+			SimpleVector2D v2 = v1 / 0.75;
+			SimpleVector2D v3 = v1;
+			v3/=0.75;
+
+			CheckEqual(v2.GetX(), v1.GetX() / 0.75 , "Error in division operator");
+			CheckEqual(v2.GetY(), v1.GetY() / 0.75 , "Error in division operator");
+			CheckEqual(v3.GetX(), v1.GetX() / 0.75 , "Error in division operator");
+			CheckEqual(v3.GetY(), v1.GetY() / 0.75 , "Error in division operator");
+
+		END_UNIT_TEST("TestOperatorDiv")
 	}
 
 	void Run()
 	{
 		TestConstructors();
 		TestOperatorAffectation();
+		TestAbs();
+		TestOperatorAdd();
+		TestOperatorSupp();
+		TestOperatorMult();
+		TestOperatorDiv();
 
-		char aux[250];
-		sprintf(aux,"%d",GetNbrOk());
-		std::cout << "<Checks Ok>"+std::string(aux)+"</Checks Ok>\n";
-		sprintf(aux,"%d",GetNbrKo());
-		std::cout << "<Checks Ko>"+std::string(aux)+"</Checks Ko>\n";
-		sprintf(aux,"%d",GetNbrOk()+GetNbrKo());
-		std::cout << "<Checks>"+std::string(aux)+"</Checks>\n";
+		PrintStat();
 
 	}
 };
