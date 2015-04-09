bf39d6eddb3eee39827fd3eb1040b5b1191e749b
machard <pascal.machard@gmail.com>
2015-04-09 17:39:47 +0200

    Create SimpleComplex.cpp

---
+++ src/SimpleComplex.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include "../../cpp_unit_test/UnitTest.h"
+#include "../../cpp_math_lib/SimpleComplex.h"
+
+using namespace prj::math;
+using namespace prj::unit_test;
+class TestSimpleComplex : public UnitTest
+{
+public :
+TestSimpleComplex() : UnitTest("TestSimpleComplex")
+{
+}
+void TestConstructors(void)
+{
+BEGIN_UNIT_TEST("TestConstructors")
+END_UNIT_TEST("TestConstructors")
+}
+
+void Run()
+{
+TestConstructors();
+PrintStat();
+}
+};
+int main(void)
+{
+TestSimpleVector2D T1;
+T1.Run();
+return 0;
+}
