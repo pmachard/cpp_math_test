a36336535a1c0abca430ba4fe12988fcffa3f96d
machard <pascal.machard@gmail.com>
2015-04-10 10:51:38 +0200

    Update ComplexTest.cpp

--- src/ComplexTest.cpp
+++ src/ComplexTest.cpp
@@ -22,14 +22,27 @@ public :
 		Complex c2(1.0,2.0);
 		Check(c2.GetReal() == 1.0 && C2.GetImag() == 2.0,"Error in the param constuctor");
 		
-		Complex c3(c2);
-		Check(c2.GetReal() == c3.GetReal() && C2.GetImag() == C3.GetImag(),"Error in the copy constuctor");
+		Complex *pc3 = new Complex(c2);
+		Check(c2.GetReal() == pc3->GetReal() && C2.GetImag() == pc3->GetImag(),"Error in the copy constuctor");
 		END_UNIT_TEST("TestConstructors")
 	}
+	
+	void TestOperatorAffectation(void)
+	{
+		BEGIN_UNIT_TEST("TestOperatorAffectation")
+		Complex v1(1.0, 2.0);
+		Complex v2,v3;
+		v3 = v2 = v1;
+		Check(v2.GetReal() == v1.GetReal() && v2.GetImag() == v1.GetImag(), "Error in affection operator");
+		Check(v3.GetReal() == v1.GetReal() && v3.GetImag() == v1.GetImag(), "Error in affection operator");
+		END_UNIT_TEST("TestOperatorAffectation")
+	}
+	
 	
 	void Run()
 	{
 		TestConstructors();
+		TestOperatorAffectation();
 		PrintStat();
 	}
 };
